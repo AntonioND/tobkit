44d8849c645d8b75a04c311f46ea5868be8271d7
weyand <weyand@3a9d4ea2-b655-11de-b129-73e2b2a9d7e9>
2009-10-11 13:15:22 +0000

    Applied LGPL

--- include/tobkit/messagebox.h
+++ include/tobkit/messagebox.h
@@ -1,3 +1,23 @@
+/*
+ * TobKit - A simple user interface toolkit for Nintendo DS homebrew
+ *                   Copyright 2005-2009 Tobias Weyand (me@tobw.net)
+ *                                   http://code.google.com/p/tobkit
+ *
+ * TobKit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * TobKit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with TobKit.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _MESSAGEBOX_H_
 #define _MESSAGEBOX_H_
 
--- source/fontchars.cpp
+++ source/fontchars.cpp
@@ -1,3 +1,23 @@
+/*
+ * TobKit - A simple user interface toolkit for Nintendo DS homebrew
+ *                   Copyright 2005-2009 Tobias Weyand (me@tobw.net)
+ *                                   http://code.google.com/p/tobkit
+ *
+ * TobKit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * TobKit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with TobKit.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 char fontchars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\"!?+-*_#%&(),.<=>/\\:[]'@$;{}^~ ";
                                 // a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9 " ! ? + - * _ # % & ( ) , . < = > / \ : [ ] ' @ $ ; { } ^ ~
 unsigned char charwidths_8x11[] = {6,6,6,6,6,4,6,6,2,4,6,2,8,6,6,6,6,4,6,4,6,7,8,7,6,6,7,7,7,7,7,7,7,7,2,7,7,7,8,7,7,7,7,7,8,7,7,7,8,8,8,7,6,6,6,6,6,6,6,6,6,6,6,2,7,6,6,6,6,7,7,7,4,4,3,2,4,4,4,7,7,2,3,3,3,6,7,3,4,4,6,8,3};
